446f86086159b4b82f9d7223bb577084efa5a0f6
unknown <bobsnake64@gmail.com>
2015-07-27 14:26:07 -0500

    Added Spell and related models.

--- Spell_List/models.py
+++ Spell_List/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Spell(models.Model):
+    name = models.CharField(max_length=255)
+    is_ritual = models.BooleanField(default=False)
+    is_concentration = models.BooleanField(default=False)
+    level = models.PositiveSmallIntegerField(default=0)
+    school = models.CharField(max_length=255)
+    casting_time = models.CharField(max_length=255)
+    duration = models.CharField(max_length=255)
+    description = models.TextField()
+
+    components = models.ManyToManyField(Component)
+    canBeCastBy = models.ManyToManyField(CharacterClass)
+
+class Component(models.Model):
+    pass
+
+class CharacterClass(models.Model):
+    pass
