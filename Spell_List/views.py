c192109130acd57ce4f6c57e4f2b01511ae141f1
unknown <bobsnake64@gmail.com>
2015-07-28 15:25:10 -0500

    Added templates, CSS.

--- Spell_List/views.py
+++ Spell_List/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render, render_to_response
+from django.shortcuts import render, render_to_response, get_list_or_404
 from Spell_List.models import Spell
 
+
 def index(request):
-    spells = Spell.objects.all()
-    return render_to_response('Spell_List/index.html', context=spells)
+    return render_to_response("Spell_List/index.html", {
+        "spells": get_list_or_404(Spell.objects.all())
+    })
