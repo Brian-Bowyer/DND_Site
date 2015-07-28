78529cb45666b560e8d5260ae295a9a6a564c7be
unknown <bobsnake64@gmail.com>
2015-07-28 13:42:14 -0500

    Added admin site.
    Also made several changes to the models, particularly Spell, based on obvious problems with the previous model revealed upon getting the admin site up.

--- Spell_List/admin.py
+++ Spell_List/admin.py
@@ -1,4 +1,15 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
-#TODO: Make admin site
+@admin.register(Spell)
+class SpellAdmin(admin.ModelAdmin):
+    list_display = ('name', 'level', 'school', 'casting_time', 'duration', 'range', 'is_ritual', 'is_concentration')
+
+@admin.register(Component)
+class ComponentAdmin(admin.ModelAdmin):
+    list_display = ('abbreviation', 'name')
+
+@admin.register(CharacterClass)
+class CharacterClassAdmin(admin.ModelAdmin):
+    pass
--- Spell_List/models.py
+++ Spell_List/models.py
@@ -1,25 +1,37 @@
 from django.db import models
 
 class Component(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length = 255, default = "")
+    abbreviation = models.CharField(max_length = 5, default = "")
 
-class CharacterClass(models.Model):
-    name = models.CharField(max_length=255)
+    def __str__(self):
+        return self.name
 
 class Spell(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length = 255)
 
-    is_ritual = models.BooleanField(default=False)
-    is_concentration = models.BooleanField(default=False)
+    is_ritual = models.BooleanField(default = False)
+    is_concentration = models.BooleanField(default = False)
 
-    level = models.PositiveSmallIntegerField(default=0)
+    level = models.PositiveSmallIntegerField(default = 0)
 
-    school = models.CharField(max_length=255)
-    casting_time = models.CharField(max_length=255)
-    duration = models.CharField(max_length=255)
+    school = models.CharField(max_length = 255, default= "")
+    casting_time = models.CharField(max_length = 255, default= "1 action") #TODO: Make a DNDTime model
+    duration = models.CharField(max_length = 255, default= "", blank=True)
+    range = models.CharField(max_length = 255, default="", blank= True)
 
-    description = models.TextField()
+    description = models.TextField(default="")
+    at_higher_levels = models.TextField(default="", blank=True)
 
     components = models.ManyToManyField(Component)
-    canBeCastBy = models.ManyToManyField(CharacterClass)
+    #TODO: Add secondary attributes damage, damage type, scaling
 
+    def __str__(self):
+        return self.name
+
+class CharacterClass(models.Model):
+    name = models.CharField(max_length = 255, default = "")
+    canCast = models.ManyToManyField(Spell)
+
+    def __str__(self):
+        return self.name
