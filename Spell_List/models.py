233fdb934fad78e5f561cc851f82acc4c065c804
unknown <bobsnake64@gmail.com>
2015-07-28 12:39:39 -0500

    Filled out Component and CharacterClass models.

--- Spell_List/models.py
+++ Spell_List/models.py
@@ -2,19 +2,23 @@ from django.db import models
 
 class Spell(models.Model):
     name = models.CharField(max_length=255)
+
     is_ritual = models.BooleanField(default=False)
     is_concentration = models.BooleanField(default=False)
+
     level = models.PositiveSmallIntegerField(default=0)
+
     school = models.CharField(max_length=255)
     casting_time = models.CharField(max_length=255)
     duration = models.CharField(max_length=255)
+
     description = models.TextField()
 
     components = models.ManyToManyField(Component)
     canBeCastBy = models.ManyToManyField(CharacterClass)
 
 class Component(models.Model):
-    pass
+    name = models.CharField(max_length=255)
 
 class CharacterClass(models.Model):
-    pass
+    name = models.CharField(max_length=255)
